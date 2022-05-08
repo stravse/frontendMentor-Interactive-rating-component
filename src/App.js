baee6b7d30d5928e3329b082a12292688159a51b
stravse <stravse@gmail.com>
2022-05-08 12:28:50 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import star from "./icon-star.svg"
+import thanksLogo from "./illustration-thank-you.svg"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      <div className="card">
+        <img src={star} alt="icon start"></img>
+        <div className="card-text">
+          <h1>How did we do?</h1>
+          <p>
+          Please let us know how we did with your support request. All feedback is appreciated 
+          to help us improve our offering!
+          </p>
+        </div>
+        <div className="rating-container">
+          1 2 3 4 5
+        </div>
+        <button className="submit-btn">Submit</button>
+      </div>
     </div>
   );
 }
