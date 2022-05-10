2397f6c52c066e1c71acc2f63a339561bf0521fb
stravse <stravse@gmail.com>
2022-05-10 22:19:33 +0800

    some stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './index.css';
+import './style.scss';
 import star from "./icon-star.svg"
 import thanksLogo from "./illustration-thank-you.svg"
 
@@ -6,18 +6,27 @@ import thanksLogo from "./illustration-thank-you.svg"
 
 function App() {
   return (
-    <div className="card-container">
-      <div className="card">
-        <img src={star} alt="icon start"></img>
+    <div className="card-container dis-flex-row">
+      <div className="card dis-flex-col">
+        <div className='star-container dis-flex-col'>
+          <img className="star-logo" src={star} alt="icon start"></img>
+        </div>
+        
         <div className="card-text">
           <h1>How did we do?</h1>
           <p>
-          Please let us know how we did with your support request. All feedback is appreciated 
-          to help us improve our offering!
+          Please let us know how we did with your support<br /> request. All feedback is appreciated 
+          to help us <br /> improve our offering!
           </p>
         </div>
         <div className="rating-container">
-          1 2 3 4 5
+          <ul>
+            <li><Button value="1" /></li>
+            <li><Button value="2" /></li>
+            <li><Button value="3" /></li>
+            <li><Button value="4" /></li>
+            <li><Button value="5" /></li>
+          </ul>
         </div>
         <button className="submit-btn">Submit</button>
       </div>
@@ -25,4 +34,20 @@ function App() {
   );
 }
 
+function Button({value}){
+  return(
+    <button className="number-value">{value}</button>
+  )
+}
+
+function ThankYou(){
+  return(
+    <div className='card-container dis-flex-col'>
+      <div className='card dis-flex-col'>
+        
+      </div>
+
+    </div>
+  )
+}
 export default App;
